8b155a11ee6683084c6ac99dd9afd1d6f7602f13
Ardhendu-official <ardhendudasofficial@gmail.com>
2022-09-26 00:32:04 +0530

    AWS Docker Update

--- app/config/database.py
+++ app/config/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL='mysql+mysqlconnector://root:mahicircle@192.168.0.140:3306/mahicircle'         #local docker
 
-SQLALCHEMY_DATABASE_URL='mysql+mysqlconnector://root:mahicircle@3.111.232.15:3306/mahicircle'        #aws docker
+SQLALCHEMY_DATABASE_URL='mysql+mysqlconnector://root:mahicircle@3.7.211.27:3306/mahicircle'        #aws docker
 
 # SQLALCHEMY_DATABASE_URL='mysql+mysqlconnector://root@localhost:3306/mahicircle'                      #localhost
 
